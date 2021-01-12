88012b3afe9d334f05e6d2518d4ea19d291a73fb
SlawekMaciejewski <slawek74.m@interia.pl>
2021-01-12 14:54:11 +0100

    Forward and backward line drawing functions have been added.

--- main.py
+++ main.py
@@ -3,4 +3,17 @@ from turtle import Turtle, Screen
 pen = Turtle()
 screen = Screen()
 
+
+def draw_ff():
+    pen.forward(10)
+
+
+def draw_back():
+    pen.backward(10)
+
+
+screen.listen()
+screen.onkeypress(key="w", fun=draw_ff)
+screen.onkeypress(key="s", fun=draw_back)
+
 screen.exitonclick()
