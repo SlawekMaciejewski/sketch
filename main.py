2cc354b3c7e0552292c123a8adf9d2dec9f9ed16
SlawekMaciejewski <slawek74.m@interia.pl>
2021-01-12 15:24:03 +0100

    Clockwise, counterclockwise functions to rotate pen and clear function have been added.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from turtle import Turtle, Screen
 
 pen = Turtle()
 screen = Screen()
+screen.title("SKETCH in Python")
 
 
 def draw_ff():
@@ -12,8 +13,26 @@ def draw_back():
     pen.backward(10)
 
 
+def clear():
+    pen.penup()
+    pen.clear()
+    pen.home()
+    pen.pendown()
+
+
+def rotate_counter_clockwise():
+    pen.left(5)
+
+
+def rotate_clockwise():
+    pen.right(5)
+
+
 screen.listen()
 screen.onkeypress(key="w", fun=draw_ff)
 screen.onkeypress(key="s", fun=draw_back)
+screen.onkeypress(key="a", fun=rotate_counter_clockwise)
+screen.onkeypress(key="d", fun=rotate_clockwise)
+screen.onkeyrelease(key="c", fun=clear)
 
 screen.exitonclick()
